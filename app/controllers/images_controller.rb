cce09f38ac833811759665939090a979e41637b3
Ryan Prairie <me@ryanprairie.com>
2021-01-22 18:56:32 -0500

    routes work

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,36 @@
 class ImagesController < ApplicationController
-  def new
+  def index
+    @images = Images.all
   end
+
+  def show
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    imagj
+    @image = Image.new
+  end
+
+  def destroy
+    @image.destroy
+    respond_to do |format| 
+      format.html { redirect_to images_url, notice: 'Image was deleted' }
+      format.json { head :no_content}
+    end
+  end
+
+  private
+
+    # Search by the id
+    def set_image_by_id
+      @image = Micropost.find(params[:imgID])
+    end
+
+    def image_params
+      params.require(:image).permit(:imgID, :data)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/images', to: redirect('/')
   get 'images/new'
+  root 'images#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '*path' => redirect('/')
+
 end
