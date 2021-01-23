5afcc4b0a25cbc58de937d67141374c572684c34
Ryan Prairie <me@ryanprairie.com>
2021-01-23 17:48:40 -0500

    some minor formatting changes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -24,9 +24,11 @@ class ImagesController < ApplicationController
     @image = Image.new
   end
 
+  # DELETE /images/1
+  # DELETE /images/1.json
   def destroy
     @image.destroy
-    respond_to do |format| 
+    respond_to do |format|
       format.html { redirect_to images_url, notice: 'Image was deleted' }
       format.json { head :no_content}
     end
