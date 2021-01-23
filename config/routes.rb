bdfcca7fac0cda3a603078f788ffbcfbfecf7baf
Ryan Prairie <me@ryanprairie.com>
2021-01-23 18:42:26 -0500

    routing table works nicely

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   get '/images', to: redirect('/')
+
   get 'images/new'
+
+  post 'images/new', to: 'images#create'
+  post 'images', to: 'images#create'
+
+  get 'images/:imgID', to: 'images#show'
+
+  delete 'images/:imgID', to: 'images#destroy'
   root 'images#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '*path' => redirect('/')
+  #get '*path' => redirect('/')
 
 end
